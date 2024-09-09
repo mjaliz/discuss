85637dfd1bc87c4943f18ec021213f75a99f9fa7
mjaliz <mrph14@gmail.com>
2024-09-09 17:48:18 +0330

    stubbing out server actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,5 @@
-"use server";
-import * as auth from "@/auth";
-
-export async function singIn() {
-  return auth.signIn("github");
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
+export { signIn } from "./sign-in";
+export { signOut } from "./sign-out";
+export { createTopic } from "./create-topic";
+export { createPost } from "./create-post";
+export { createComment } from "./create-comment";
