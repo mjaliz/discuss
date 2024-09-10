9148fdae63b11fcaf8e161e514e314d12d473f6f
mjaliz <mrph14@gmail.com>
2024-09-10 21:13:21 +0330

    working on form

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,31 @@
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+
 export default function TopicCreateForm() {
-  return <div>Create Topic From</div>;
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Topic</h3>
+            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Textarea
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Dedcribe your topic"
+            />
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
 }
